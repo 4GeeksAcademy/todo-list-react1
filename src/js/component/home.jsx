1f7087b783a47d68575a8b39a19da8c5a0fcf065
kazou92 <99810202+kazou92@users.noreply.github.com>
2025-01-26 01:51:03 +0000

    todo list project loaded

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+
+
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
+	const [todo, setTodo] = useState([]);
+	const [inputValue, setInputValue] = useState("");
+	const agregar = (e) => {
+	  e.preventDefault();
+	  if (inputValue.trim() !== "") {
+		setTodo([...todo, inputValue]);
+		setInputValue("");
+	  }
+	};
+	console.log(todo);
+	const handleKeyDown = (e) => {
+	  if (e.key === "Enter") {
+		agregar(e);
+	  }
+	};
+	const eliminar = (item)=>{
+		setTodo(todo.filter((_, i)=> i !== item))
+		
+	}
+  
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+	  <div className="mx-auto mt-3 p-3 shadow p-3 mb-5 bg-body-tertiary rounded"  style={{width:"590px"}}>
+		<form action="" onSubmit={agregar} className="form">
+		  <div className="mb-3">
+			<label htmlFor="Input1" className="form-label"></label>
+			<input
+			  value={inputValue}
+			  onKeyDown={handleKeyDown}
+			  onChange={(e) => setInputValue(e.target.value)}
+			  type="text"
+			  className="form-control "
+			  id="Input1"
+			  placeholder="name"
+			/>
+		  </div>
+		  <ul className="list-group">
+
+		  {todo.map((t, i) => (
+			<li className="list-group-item d-flex justify-content-between" key={i}> {t} 
+			<button className="border border-0 bg-white" onClick={()=>eliminar(i)} > X </button>
+			 </li>  
+			
+			
+		))}{" "}
+		
+		  </ul>
+		</form>
+		<span> {todo.length} Items left  <i class="bi bi-x-circle-fill"></i> </span>
+	  </div>
 	);
-};
+  };
+
 
 export default Home;
